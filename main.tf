172da4136eb3130d72af5e7a112c90db4c0f2c85
Nicholas Green <nickgreenweb@gmail.com>
2022-01-20 10:35:19 +0100

    Changed ssh keys variables
    Reworked to pass in ssh keys as file contents instead of file paths

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "proxmox_lxc" "server"  {
     startup = "order=1,up=5" # order is priority, up is how long to wait before startingthe next container
     #TODO add startup order variable: startup = ....
     ssh_public_keys = <<-EOT
-        ${file(var.vm_connection_details.pub)}
+        ${var.vm_connection_details.pub}
     EOT
 
 
@@ -80,28 +80,28 @@ resource "proxmox_lxc" "server"  {
     connection {
         host = var.ip        
         user = "${ var.vm_connection_details.user }"
-        private_key = file(var.vm_connection_details.priv)
+        private_key = var.vm_connection_details.priv
         agent = false
         timeout = "5m"
     } 
 }
 
-module "create_ansible_user"{
-    depends_on = [ proxmox_lxc.server]
-    #count      = length( proxmox_lxc.server)
-    source= "github.com/nickgreensgithub/tf_module_create_remote_user"
+# module "create_ansible_user"{
+#     depends_on = [ proxmox_lxc.server]
+#     #count      = length( proxmox_lxc.server)
+#     source= "github.com/nickgreensgithub/tf_module_create_remote_user"
 
-    connection = {
-            ip = var.ip
-            user= var.vm_connection_details.user
-            private_key = var.vm_connection_details.priv
-    }
-    user = {
-            name = "${ var.create_user.user }"
-            is_sudo = true
-            public_ssh="${ var.create_user.pub }"
-    }
-}
+#     connection = {
+#             ip = var.ip
+#             user= var.vm_connection_details.user
+#             private_key = var.vm_connection_details.priv
+#     }
+#     user = {
+#             name = "${ var.create_user.user }"
+#             is_sudo = true
+#             public_ssh="${ var.create_user.pub }"
+#     }
+# }
 
 
 
